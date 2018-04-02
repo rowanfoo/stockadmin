b71c129cf91023c25a3f2ff85acf5b15941e8910
rowanfoo <rowanfoo@gmail.com>
2018-04-02 21:31:38 +0800

    2nd commit

--- src/main/java/com/dana/admin/stockadmin/controller/admin/AsxMetaStockImport.java
+++ src/main/java/com/dana/admin/stockadmin/controller/admin/AsxMetaStockImport.java
@@ -122,7 +122,7 @@ private  void   insertdata(String code)throws Exception{
 
 }
 
-    @Scheduled(cron = "0 21 05 ? * MON-FRI")
+    @Scheduled(cron = "0 21 33 ? * MON-FRI")
     public void  importAllData() {
         System.out.println("----ASX import RUN !!!!!  --:");
         //allasxcodes.forEach((a)-> System.out.println("----codes--:"+a));
