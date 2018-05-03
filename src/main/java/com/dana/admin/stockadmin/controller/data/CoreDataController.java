c9761cb7d27a838c0f562e9b6a430618dff61bfe
rowanfoo <rowanfoo@gmail.com>
2018-05-04 00:30:47 +0800

    Add debugging message

--- src/main/java/com/dana/admin/stockadmin/controller/data/CoreDataController.java
+++ src/main/java/com/dana/admin/stockadmin/controller/data/CoreDataController.java
@@ -2,13 +2,18 @@ package com.dana.admin.stockadmin.controller.data;
 
 import com.dana.admin.stockadmin.data.entity.CoreData;
 import com.dana.admin.stockadmin.service.data.CoreDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/data")
 public class CoreDataController {
-
+    protected Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     CoreDataService coreDataService;
 
@@ -17,7 +22,7 @@ public class CoreDataController {
     //@CrossOrigin(origins = "http://localhost:8090")
     @CrossOrigin
     public Iterable<CoreData> getDataFrom( @RequestParam String code,@RequestParam String date ){
-
+        logger.info("----------------getDataFrom :  code  : ",code );
         return coreDataService.getDataFrom(code,date );
     }
 
