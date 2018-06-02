2fd083626de44b637a8e622eeaafde00bbc97522
rowanfoo <rowanfoo@gmail.com>
2018-06-02 23:10:46 +0800

    2nd commit

--- src/main/java/com/dana/admin/stockadmin/data/entity/FundNews.java
+++ src/main/java/com/dana/admin/stockadmin/data/entity/FundNews.java
@@ -1,5 +1,6 @@
 package com.dana.admin.stockadmin.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,17 +8,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 
 @Entity
 @Getter
 @Setter
 public class FundNews {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String code;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+
     private java.time.LocalDate date;
     private String link ;
     private String title;
@@ -28,10 +32,26 @@ public class FundNews {
 
     public  FundNews(){}
 
-    public FundNews(String code, LocalDate date, String title, String link) {
-        this.code = code;
-        this.date = date;
-        this.link = link;
-        this.title = title;
-    }
+//    public FundNews(String code, LocalDate date, String title, String link) {
+//        this.code = code;
+//        this.date = date;
+//        this.link = link;
+//        this.title = title;
+//    }
+@Override
+public String toString() {
+    return "FundNews{" +
+            "id=" + id +
+            ", code='" + code + '\'' +
+            ", date=" + date +
+            ", link='" + link + '\'' +
+            ", title='" + title + '\'' +
+            ", seen='" + seen + '\'' +
+            ", ok='" + ok + '\'' +
+            ", notes='" + notes + '\'' +
+            ", yesnotes='" + yesnotes + '\'' +
+            '}';
+}
+
+
 }
